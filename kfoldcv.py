f11ff8c00ab24af8fe9a0c0df16ec106d2f66dc5
Francesco Franco <130352141+Francesco601@users.noreply.github.com>
2023-12-04 14:40:18 +0100

    program to ilustrate use of K-fold Cross Valdation
    K-fold Cross Validation in machine learning is way of ensuring accuracy of predictions , generalizability, and avoiding overfitting/underfitting..

---
+++ kfoldcv.py
@@ -0,0 +1,171 @@
+import os
+import torch
+from torch import nn
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, ConcatDataset
+from torchvision import transforms
+from sklearn.model_selection import KFold
+
+def reset_weights(m):
+  '''
+    Try resetting model weights to avoid
+    weight leakage.
+  '''
+  for layer in m.children():
+   if hasattr(layer, 'reset_parameters'):
+    print(f'Reset trainable parameters of layer = {layer}')
+    layer.reset_parameters()
+
+class SimpleConvNet(nn.Module):
+  '''
+    Simple Convolutional Neural Network
+  '''
+  def __init__(self):
+    super().__init__()
+    self.layers = nn.Sequential(
+      nn.Conv2d(1, 10, kernel_size=3),
+      nn.ReLU(),
+      nn.Flatten(),
+      nn.Linear(26 * 26 * 10, 50),
+      nn.ReLU(),
+      nn.Linear(50, 20),
+      nn.ReLU(),
+      nn.Linear(20, 10)
+    )
+
+
+  def forward(self, x):
+    '''Forward pass'''
+    return self.layers(x)
+  
+  
+if __name__ == '__main__':
+  
+  # Configuration options
+  k_folds = 5
+  num_epochs = 1
+  loss_function = nn.CrossEntropyLoss()
+  
+  # For fold results
+  results = {}
+  
+  # Set fixed random number seed
+  torch.manual_seed(42)
+  
+  # Prepare MNIST dataset by concatenating Train/Test part; we split later.
+  dataset_train_part = MNIST(os.getcwd(), download=True, transform=transforms.ToTensor(), train=True)
+  dataset_test_part = MNIST(os.getcwd(), download=True, transform=transforms.ToTensor(), train=False)
+  dataset = ConcatDataset([dataset_train_part, dataset_test_part])
+  
+  # Define the K-fold Cross Validator
+  kfold = KFold(n_splits=k_folds, shuffle=True)
+    
+  # Start print
+  print('--------------------------------')
+
+  # K-fold Cross Validation model evaluation
+  for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
+    
+    # Print
+    print(f'FOLD {fold}')
+    print('--------------------------------')
+    
+    # Sample elements randomly from a given list of ids, no replacement.
+    train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+    test_subsampler = torch.utils.data.SubsetRandomSampler(test_ids)
+    
+    # Define data loaders for training and testing data in this fold
+    trainloader = torch.utils.data.DataLoader(
+                      dataset, 
+                      batch_size=10, sampler=train_subsampler)
+    testloader = torch.utils.data.DataLoader(
+                      dataset,
+                      batch_size=10, sampler=test_subsampler)
+    
+    # Init the neural network
+    network = SimpleConvNet()
+    network.apply(reset_weights)
+    
+    # Initialize optimizer
+    optimizer = torch.optim.Adam(network.parameters(), lr=1e-4)
+    
+    # Run the training loop for defined number of epochs
+    for epoch in range(0, num_epochs):
+
+      # Print epoch
+      print(f'Starting epoch {epoch+1}')
+
+      # Set current loss value
+      current_loss = 0.0
+
+      # Iterate over the DataLoader for training data
+      for i, data in enumerate(trainloader, 0):
+        
+        # Get inputs
+        inputs, targets = data
+        
+        # Zero the gradients
+        optimizer.zero_grad()
+        
+        # Perform forward pass
+        outputs = network(inputs)
+        
+        # Compute loss
+        loss = loss_function(outputs, targets)
+        
+        # Perform backward pass
+        loss.backward()
+        
+        # Perform optimization
+        optimizer.step()
+        
+        # Print statistics
+        current_loss += loss.item()
+        if i % 500 == 499:
+            print('Loss after mini-batch %5d: %.3f' %
+                  (i + 1, current_loss / 500))
+            current_loss = 0.0
+            
+    # Process is complete.
+    print('Training process has finished. Saving trained model.')
+
+    # Print about testing
+    print('Starting testing')
+    
+    # Saving the model
+    save_path = f'./model-fold-{fold}.pth'
+    torch.save(network.state_dict(), save_path)
+
+    # Evaluationfor this fold
+    correct, total = 0, 0
+    with torch.no_grad():
+
+      # Iterate over the test data and generate predictions
+      for i, data in enumerate(testloader, 0):
+
+        # Get inputs
+        inputs, targets = data
+
+        # Generate outputs
+        outputs = network(inputs)
+
+        # Set total and correct
+        _, predicted = torch.max(outputs.data, 1)
+        total += targets.size(0)
+        correct += (predicted == targets).sum().item()
+
+      # Print accuracy
+      print('Accuracy for fold %d: %d %%' % (fold, 100.0 * correct / total))
+      print('--------------------------------')
+      results[fold] = 100.0 * (correct / total)
+    
+  # Print fold results
+  print(f'K-FOLD CROSS VALIDATION RESULTS FOR {k_folds} FOLDS')
+  print('--------------------------------')
+  sum = 0.0
+  for key, value in results.items():
+    print(f'Fold {key}: {value} %')
+    sum += value
+  print(f'Average: {sum/len(results.items())} %')
+
+  
